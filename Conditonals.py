01eb565cc977690ee013ab1eaa54ce46f3719322
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-04 11:04:04 +0100

    Conditional Tutorial and Exercise

---
+++ Conditonals.py
@@ -0,0 +1,18 @@
+devs_money = 100
+dev_can_play_smash = False
+if devs_money > 10 and dev_can_play_smash:
+    print("Dev enters a smash tournament!")
+elif devs_money < 10 and dev_can_play_smash:
+    print("Dev is too poor to enter")
+else:
+    print("Dev just can't play smash")
+
+
+intMark = int(input("Please enter your mark"))
+if intMark >=85:
+    print("Distintion, Good job")
+elif intMark > 65:
+    print("Pass, congrats dude")
+else:
+    print("Fail, sorry man")
+
--- mytest.py
+++ mytest.py
@@ -1,1 +1,5 @@
-print("I can python")
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1,"+",number2,"=",answer)
