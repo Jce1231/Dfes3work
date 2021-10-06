a32a155ff8047bfdbf9a489d251706360bf9f779
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-06 10:24:43 +0100

    Started Bebugging

---
+++ n_Debugme.py
@@ -0,0 +1,45 @@
+# user_funds = 10.31
+# item_price = price["Burger"]
+####Create a dictionary for price{"burger":10}
+# if item_price < user_funds:
+#     Print("You have enough money!")
+# if item_price = user_funds: #Assigment equals, not comparison
+#     print("You have the precise amount of money") 
+# if item_price < user_funds: 
+#     print(Sorry you don't have enough money) #Missing Quotes on print statement
+
+
+# def product(n):
+#     total == 1 #Comparison statement equals
+#     for n in n: #Reused variable names
+#         total *= i  #i is not a variable in this code, not sure if *= works.
+# return total 
+
+# print(product([4,4,5])) #Product only takes one input.
+
+
+
+# import pdb
+# pdb.set_trace()
+# def is_prime(x):
+#     if x < 2:
+#         return False
+#     else:
+#         for n in range(2, x-1):
+#             if x % n == 0:
+#                 return False
+#             return True #Indentation Erorr, will return True in the loop
+
+
+
+
+
+
+# item_list = ["Burger", "Hotdog", "Bun", "Ketchup", "Cheese"]
+# n = 0
+
+# while n < 5:
+#     for i in item_list: #i the variable, alternative use in range.
+#         print(item_list[i]) #Using print(i) may solve this.
+
+# print(item_list[5]) #Added Brackets to print statement
