7bffa9e2233b9f057d280a6ec9e480a2914020dd
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-08 11:07:15 +0100

    Added color Coded output for deposit/withdraw

--- BudgetApp.py
+++ BudgetApp.py
@@ -1,5 +1,6 @@
 from BudgetAppClass import CategoryBudget
 from random import randint
+from termcolor import colored
 budgetCategory = [
 CategoryBudget(200,'food'),
 CategoryBudget(200,'clothing'),
@@ -7,6 +8,10 @@ CategoryBudget(500,'bills'),
 CategoryBudget(50,'entertainment'),
 CategoryBudget(400,'car')
 ]
+##Plan use file in and file out,
+# use a folder to store the output for each category type.
+#Code to detect the categorys
+#GUI for easier user visuality? color code green for money in, red for out.
 
 
 for i in range(5):
@@ -18,5 +23,10 @@ for i in range(5):
 
 budgetCategory[0].transfer(100,budgetCategory[2])
 for i in budgetCategory:
-    print(f"The ledger for {i.category} is: {i.ledger()}")
+    print(f"The ledger for {i.category} is: ")
+    for j in i.ledger():
+        if j >0:
+            print(f"{colored(j,'green')}")
+        else:
+            print(f"{colored(j,'red')}")
     print(i.balance_())
